34805358678a6d24a639bc458bf45ed2b912c28f
SHINDAESUB <weotjqw@naver.com>
2024-01-19 16:30:51 +0900

    feat: 스타일드 컴포넌트를 적용한다

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  compiler: {
+    styledComponents: true, // 스타일드 컴포넌트를 활성화 한다
+  }
+}
+
+module.exports = nextConfig
