d46607851502aa5d02783dbaefefda7038d07066
SHINDAESUB <weotjqw@naver.com>
2024-01-19 16:36:46 +0900

    feat: ESLint를 설정한다

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
-import { AppProps } from 'next/app'
-import Head from 'next/head'
-import { createGlobalStyle } from 'styled-components'
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+import { createGlobalStyle } from 'styled-components';
 
 // 글로벌 스타일
 const GlobalStyle = createGlobalStyle`
@@ -27,23 +27,23 @@ a {
 ol, ul {
   list-style: none;
 }
-`
+`;
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  return (
-    <>
-      <Head>
-        <meta key="charset" name="charset" content="utf-8" />
-        <meta
-          key="viewport"
-          name="viewport"
-          content="width=device-width, initial-scale=1, shrink-to-fit=no, maximum-scale=5"
-        />
-      </Head>
-      <GlobalStyle />
-      <Component {...pageProps}/>
-    </>
-  )
-}
+    return (
+        <>
+            <Head>
+                <meta key="charset" name="charset" content="utf-8" />
+                <meta
+                    key="viewport"
+                    name="viewport"
+                    content="width=device-width, initial-scale=1, shrink-to-fit=no, maximum-scale=5"
+                />
+            </Head>
+            <GlobalStyle />
+            <Component {...pageProps} />
+        </>
+    );
+};
 
-export default MyApp
+export default MyApp;
