5c2b6ac7710d9cab9282cd4a5ffdb7e42bb26a9f
SHINDAESUB <weotjqw@naver.com>
2024-01-25 14:40:20 +0900

    feat: 스토리북 설정파일을 수정한다

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,6 @@
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const path = require('path');
+
 module.exports = {
   stories: [
     '../src/**/*.stories.mdx',
@@ -7,6 +10,24 @@ module.exports = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-postcss',
-  ],  
+  ],
   staticDirs: ['public'],
-}
+  babel: async options => ({
+    ...options,
+    plugins: [
+      '@babel/plugin-proposal-class-properties',
+      '@babel/plugin-proposal-private-methods',
+      '@babel/plugin-proposal-private-property-in-object',
+    ],
+  }),
+  // tsconfig 설정을 상속한다 
+  webpackFinal: async (config) => {
+    config.resolve.plugins = [
+      new TsconfigPathsPlugin({
+        configFile: path.resolve(__dirname, '../tsconfig.json')
+      }),
+    ];
+
+    return config
+  },
+};
