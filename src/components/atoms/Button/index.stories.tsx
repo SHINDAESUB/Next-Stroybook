9b768fc0663ee42fef87af60ea646e8477bf40ee
SHINDAESUB <weotjqw@naver.com>
2024-01-29 17:11:01 +0900

    style: 'Button' 스토리북에 주석을 추가한다

--- src/components/atoms/Button/index.stories.tsx
+++ src/components/atoms/Button/index.stories.tsx
@@ -2,18 +2,23 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 import Button from './index';
 
+/**
+ * @description 스토리북에서 제공하는 'Docs' 탭에서 문서를 추가한다
+ * @property {string} title - 제목 ex_ '대분류'/ '중분류'
+ * @property {Object} argTypes - 컴포넌트의 각 프로퍼티(매개변수)에 대한 타입 및 컨트롤을 정의합니다.
+ */
 export default {
     title: 'Atoms/Button',
     argTypes: {
         variant: {
-            options: ['primary', 'secondary', 'danger'],
-            control: { type: 'radio' },
-            defaultValue: 'primary',
-            description: '버튼 형태',
+            options: ['primary', 'secondary', 'danger'], // 선택 가능한 옵션
+            control: { type: 'radio' }, // 제어 타입
+            defaultValue: 'primary', // 기본 선택값
+            description: '버튼 형태', // 해당 기능 설명
             table: {
                 type: { summary: 'primary | secondary | danger' },
                 defaultValue: { summary: 'primary' },
-            },
+            }, // 제어 타입에 들어갈 옵셔널
         },
         children: {
             control: { type: 'text' },
@@ -54,6 +59,9 @@ export default {
     },
 } as ComponentMeta<typeof Button>;
 
+/**
+ * @description 구현 템플릿을 제공한다
+ */
 const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 
 // Primary 버튼
