a3a6835831ccf0a25771af0a49b04ab29c964da3
SHINDAESUB <weotjqw@naver.com>
2024-02-05 13:59:02 +0900

    feat: '드롭다운' 몰리큘를 구현한다

---
+++ src/components/molecules/Dropdown/index.tsx
@@ -0,0 +1,217 @@
+import React, { useEffect, useState, useRef, useCallback } from 'react';
+import styled from 'styled-components';
+import Text from 'components/atoms/Text';
+import Flex from 'components/layout/Flex';
+
+const DropdownRoot = styled.div`
+    position: relative;
+    height: 38px;
+`;
+
+// 드롭다운 형태
+const DropdownControl = styled.div<{ hasError?: boolean }>`
+    position: relative;
+    overflow: hidden;
+    background-color: #ffffff;
+    border: ${({ theme, hasError }) =>
+        hasError
+            ? `1px solid ${theme.colors.danger}`
+            : `1px solid ${theme.colors.border}`};
+    border-radius: 5px;
+    box-sizing: border-box;
+    cursor: default;
+    outline: none;
+    padding: 8px 52px 8px 12px;
+`;
+
+const DropdownValue = styled.div`
+    color: ${({ theme }) => theme.colors.text};
+`;
+
+// 드롭다운 플레이스홀더
+const DropdownPlaceholder = styled.div`
+    color: #757575;
+    font-size: ${({ theme }) => theme.fontSizes[1]};
+    min-height: 20px;
+    line-height: 20px;
+`;
+
+// 드롭다운 화살표의 형태
+const DropdownArrow = styled.div<{ isOpen?: boolean }>`
+    border-color: ${({ isOpen }) =>
+        isOpen
+            ? 'transparent transparent #222222;'
+            : '#222222 transparent transparent'};
+    border-width: ${({ isOpen }) => (isOpen ? '0 5px 5px' : '5px 5px 0;')};
+    border-style: solid;
+    content: ' ';
+    display: block;
+    height: 0;
+    margin-top: -ceil(2.5);
+    position: absolute;
+    right: 10px;
+    top: 16px;
+    width: 0;
+`;
+
+const DropdownMenu = styled.div`
+    background-color: #ffffff;
+    border: ${({ theme }) => theme.colors.border};
+    box-shadow: 0px 5px 5px -3px rgb(0 0 0 / 20%),
+        0px 8px 10px 1px rgb(0 0 0 / 10%), 0px 3px 14px 2px rgb(0 0 0 / 12%);
+    box-sizing: border-box;
+    border-radius: 5px;
+    margin-top: -1px;
+    max-height: 200px;
+    overflow-y: auto;
+    position: absolute;
+    top: 100%;
+    width: 100%;
+    z-index: 1000;
+`;
+
+const DropdownOption = styled.div`
+    padding: 8px 12px 8px 12px;
+    &:hover {
+        background-color: #f9f9f9;
+    }
+`;
+
+interface DropdownItemProps {
+    item: DropdownItem;
+}
+
+const DropdownItem = (props: DropdownItemProps) => {
+    const { item } = props;
+
+    return (
+        <Flex alignItems="center">
+            <Text margin={0} variant="small">
+                {item.label ?? item.value}
+            </Text>
+        </Flex>
+    );
+};
+
+export interface DropdownItem {
+    value: string | number | null;
+    label?: string;
+}
+
+interface DropdownProps {
+    options: DropdownItem[]; //드롭다운 아이템
+    value?: string | number; // 값
+    name?: string;
+    placeholder?: string;
+    hasError?: boolean;
+    onChange?: (selected?: DropdownItem) => void;
+}
+
+// 드롭다운 구현체
+const Dropdown = (props: DropdownProps) => {
+    const { onChange, name, value, options, hasError } = props;
+    const initialItem = options.find((i) => i.value === value);
+    const [isOpen, setIsOpenValue] = useState(false);
+    const [selectedItem, setSelectedItem] = useState(initialItem);
+    const dropdownRef = useRef<HTMLDivElement>(null);
+
+    const handleDocumentClick = useCallback(
+        (e: MouseEvent | TouchEvent) => {
+            // 자신을 클릭할경우 선택하지 않는다
+            if (dropdownRef.current) {
+                const elems = dropdownRef.current.querySelectorAll('*');
+
+                for (let i = 0; i < elems.length; i++) {
+                    if (elems[i] == e.target) {
+                        return;
+                    }
+                }
+            }
+
+            setIsOpenValue(false);
+        },
+        [dropdownRef],
+    );
+
+    const handleMouseDown = (e: React.SyntheticEvent) => {
+        setIsOpenValue((isOpen) => !isOpen);
+        e.stopPropagation();
+    };
+
+    const handleSelectValue = (
+        e: React.FormEvent<HTMLDivElement>,
+        item: DropdownItem,
+    ) => {
+        e.stopPropagation();
+
+        setSelectedItem(item);
+        setIsOpenValue(false);
+        onChange && onChange(item);
+    };
+
+    useEffect(() => {
+        // 화면 밖 클릭 이벤트 설정
+        document.addEventListener('click', handleDocumentClick, false);
+        // 화면 밖 터치 이벤트 설정
+        document.addEventListener('touchend', handleDocumentClick, false);
+
+        return function cleanup() {
+            document.removeEventListener('click', handleDocumentClick, false);
+            document.removeEventListener(
+                'touchend',
+                handleDocumentClick,
+                false,
+            );
+        };
+        // `useEffect` 최초에만 호출
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    return (
+        <DropdownRoot ref={dropdownRef}>
+            <DropdownControl
+                hasError={hasError}
+                onMouseDown={handleMouseDown}
+                onTouchEnd={handleMouseDown}
+                data-testid="dropdown-control"
+            >
+                {selectedItem && (
+                    <DropdownValue>
+                        <DropdownItem item={selectedItem} />
+                    </DropdownValue>
+                )}
+                {/* 플레이스 홀더 호출 */}
+                {!selectedItem && (
+                    <DropdownPlaceholder>
+                        {props?.placeholder}
+                    </DropdownPlaceholder>
+                )}
+                {/* 더미 입력 */}
+                <input
+                    type="hidden"
+                    name={name}
+                    value={selectedItem?.value ?? ''}
+                    onChange={() => onChange && onChange(selectedItem)}
+                />
+                <DropdownArrow isOpen={isOpen} />
+            </DropdownControl>
+            {/* 드롭다운 표시 */}
+            {isOpen && (
+                <DropdownMenu>
+                    {props.options.map((item, idx) => (
+                        <DropdownOption
+                            key={idx}
+                            onMouseDown={(e) => handleSelectValue(e, item)}
+                            onClick={(e) => handleSelectValue(e, item)}
+                            data-testid="dropdown-option"
+                        >
+                            <DropdownItem item={item} />
+                        </DropdownOption>
+                    ))}
+                </DropdownMenu>
+            )}
+        </DropdownRoot>
+    );
+};
+
+export default Dropdown;
